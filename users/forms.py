2811d2041fd37ffbf1b536836eb7c15a7bd9b1b7
Joe Alphonso <jalphonso@juniper.net>
2020-01-23 18:35:16 -0500

    user reset password is more obvious in admin panel

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 # users/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 from .models import CustomUser
 
 
@@ -12,6 +12,11 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
+
+    password = ReadOnlyPasswordHashField(label=("Password"),
+                                         help_text=("Raw passwords are not stored, so there is no way to see "
+                                                    "this user's password, but you can change the password "
+                                                    "here: <a href=\"../password/\">RESET PASSWORD</a>."))
 
     class Meta:
         model = CustomUser
