af35a973f04f932555dbf919f433a6b14cedc3d3
Joe Alphonso <jalphonso@juniper.net>
2020-04-07 17:56:20 -0400

    removed extra logging for jsa

--- jsa/views.py
+++ jsa/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from .models import Jsa
-from django.contrib.auth.decorators import login_required
-from lib.utilities import do_user_logging
 
 
 def index(request):
     jsa_list = Jsa.objects.all().order_by('date')
 
     context = {'documents': jsa_list}
-    do_user_logging(request)
     return render(request, 'jsa/index.html', context)
