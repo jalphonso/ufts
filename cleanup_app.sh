f9538bffaff573746739cb112b978d07308b0bb8
Joe Alphonso <jalphonso@juniper.net>
2020-04-27 08:53:35 -0400

    activate venv in cleanup script

--- cleanup_app.sh
+++ cleanup_app.sh
@@ -3,8 +3,8 @@
 docker stop consul
 docker stop registrator
 docker stop consul-tpl
-docker-compose down --remove-orphans
+. .venv/bin/activate && docker-compose down --remove-orphans
 docker rm consul
 docker rm registrator
 docker rm consul-tpl
-docker-compose down
+. .venv/bin/activate && docker-compose down
