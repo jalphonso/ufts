d21408f723ec4ecaf88cc53fd57f97800298b212
Joe Alphonso <jalphonso@juniper.net>
2020-05-07 11:15:01 -0400

    change restart policy and add static ips

--- init_app.sh
+++ init_app.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -d -p 8500:8500 --name=consul --restart unless-stopped \
+docker run -d -p 8500:8500 --name=consul --restart always --net=ufts_service_discovery_network --ip 172.20.0.2\
     consul agent -server -bootstrap-expect 1 -node=myconsulnode  -client=0.0.0.0 -ui
 
-docker run -d --restart unless-stopped --name=registrator --net=host \
+docker run -d --restart always --name=registrator --net=host \
     -v /var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator:master \
     -internal \
     -useIpFromLabel=service_ip \
-    consul://`docker inspect consul --format {{.NetworkSettings.Networks.bridge.IPAddress}}`:8500
+    consul://`docker inspect consul --format {{.NetworkSettings.Networks.ufts_service_discovery_network.IPAddress}}`:8500
