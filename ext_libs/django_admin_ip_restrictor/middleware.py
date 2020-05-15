40e409dd6d58eea2a6bab1a8d3cf4a73abce6a32
Joe Alphonso <jalphonso@juniper.net>
2020-05-15 17:06:10 -0400

    ipware .ip2 suffix not needed

--- ext_libs/django_admin_ip_restrictor/middleware.py
+++ ext_libs/django_admin_ip_restrictor/middleware.py
@@ -1,5 +1,5 @@
 import ipaddress
-from ipware.ip2 import get_client_ip
+from ipware import get_client_ip
 
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden
