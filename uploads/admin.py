376ebfc5c3ff241bce755788bc6092c3913ec07a
Joe Alphonso <jalphonso@juniper.net>
2020-05-19 09:00:00 -0400

    fixed formatting of uploads/admin.py

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django import forms
 from django.http import HttpResponseRedirect
-from django.db.models import Q 
+from django.db.models import Q
 from django.contrib import messages
 from .models import UploadFile, Products
 import logging
@@ -24,13 +24,14 @@ class UploadFileAdmin(admin.ModelAdmin):
             if not obj.verified_by and obj.uploaded_by != request.user:
                 obj.verified_by=request.user
                 obj.save()
-                messages.add_message(request,messages.SUCCESS, 'File verified by {}.'.format(request.user)) 
-                logger.debug('user: {} |ip address: {} |verified_file: {}'.format(obj.verified_by, client_ip, obj.file))
+                messages.add_message(request,messages.SUCCESS, f'File verified by {request.user}.')
+                logger.debug(f'user: {obj.verified_by} |ip address: {client_ip} |verified_file: {obj.file}')
             else:
                 if obj.verified_by:
                     messages.add_message(request,messages.ERROR, 'File is already verified.')
                 else:
-                    messages.add_message(request,messages.ERROR, 'File cannot be verified by {} as you are the uploader.'.format(request.user)) 
+                    messages.add_message(request,messages.ERROR,
+                                         f'File cannot be verified by {request.user} as you are the uploader.')
 
             return HttpResponseRedirect(".")
         return super().response_change(request, obj)
@@ -40,19 +41,24 @@ class UploadFileAdmin(admin.ModelAdmin):
         notuploader=queryset.filter(~Q(uploaded_by = request.user))
         validateable=notuploader.filter(verified_by__isnull = True)
         if validateable.count() == 0:
-            messages.add_message(request,messages.ERROR, 'None of the selected files can be verified by {}. Either files were already verified, or they were uploaded by {}'.format(request.user,request.user))
+            messages.add_message(request,messages.ERROR,
+                                 f'None of the selected files can be verified by {request.user}. '
+                                 f'Either files were already verified, or they were uploaded by {request.user}')
         elif validateable.count() != queryset.count():
-            messages.add_message(request,messages.WARNING, '{} file(s) verified. {} cannot be verified by {}. Either files were already verified, or they were uploaded by {}'.format(validateable.count(),queryset.count()-validateable.count(),request.user,request.user))
+            messages.add_message(request,messages.WARNING,
+                                 f'{validateable.count()} file(s) verified. '
+                                 f'{queryset.count()-validateable.count()} cannot be verified by {request.user}. '
+                                 f'Either files were already verified, or they were uploaded by {request.user}')
             for validated in validateable:
-                logger.debug('user: {} |ip address: {} |verified_file: {}'.format(validated.verified_by, client_ip, validated.file))
+                logger.debug(f'user: {validated.verified_by} |ip address: {client_ip} |verified_file: {validated.file}')
             validateable.update(verified_by=request.user)
         else:
-            messages.add_message(request, messages.SUCCESS, '{} file(s) have been verified'.format(validateable.count()))
+            messages.add_message(request, messages.SUCCESS, f'{validateable.count()} file(s) have been verified')
             for validated in validateable:
-                logger.debug('user: {} |ip address: {} |verified_file: {}'.format(validated.verified_by, client_ip, validated.file))
+                logger.debug(f'user: {validated.verified_by} |ip address: {client_ip} |verified_file: {validated.file}')
             validateable.update(verified_by=request.user)
     verify_files.short_description = "Verify selected uploaded files"
-    
+
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = self.readonly_fields
         readonly_fields = readonly_fields + ('uploaded_by','verified_by')
@@ -69,18 +75,19 @@ class UploadFileAdmin(admin.ModelAdmin):
             fixed_name=obj.file.name.replace(' ','_')
             if obj.release_notes:
                 fixed_relnotes=obj.release_notes.name.replace(' ','_')
-                logger.debug('user: {} |ip address: {} |uploaded_file: {} |uploaded_release_notes: {}'.format(obj.uploaded_by, client_ip, fixed_name, fixed_relnotes))
+                logger.debug(f'user: {obj.uploaded_by} |ip address: {client_ip} |uploaded_file: {fixed_name} '
+                             f'|uploaded_release_notes: {fixed_relnotes}')
             else:
-                logger.debug('user: {} |ip address: {} |uploaded_file: {}'.format(obj.uploaded_by, client_ip, fixed_name))
+                logger.debug(f'user: {obj.uploaded_by} |ip address: {client_ip} |uploaded_file: {fixed_name}')
         else:
             if 'release_notes' in form.changed_data:
                 fixed_relnotes=obj.release_notes.name.replace(' ','_')
-                logger.debug('user: {} |ip address: {} |uploaded_release_notes: {}'.format(request.user, client_ip, fixed_relnotes))
+                logger.debug(f'user: {request.user} |ip address: {client_ip} |uploaded_release_notes: {fixed_relnotes}')
         super().save_model(request, obj, form, change)
 
     def delete_model(self, request, obj):
         client_ip, is_routable = get_client_ip(request)
-        logger.debug('user: {} |ip address: {} |deleted_file: {}'.format(obj.uploaded_by, client_ip, obj.file))
+        logger.debug(f'user: {obj.uploaded_by} |ip address: {client_ip} |deleted_file: {obj.file}')
         super().delete_model(request, obj)
 
 @admin.register(Products)
