48f05b62fb03871aac93a6730e9dc276f5a25533
Joe Alphonso <jalphonso@juniper.net>
2020-05-23 19:48:58 -0400

    captivating new user until profile is complete

--- users/models.py
+++ users/models.py
@@ -5,15 +5,22 @@ from lib.mixins import ModelDiffMixin
 
 
 class CustomUser(AbstractUser, ModelDiffMixin):
-    unclas_email = models.CharField(max_length=50, default='', editable=True)
+    first_name = models.CharField(max_length=50, default='', editable=True)
+    last_name = models.CharField(max_length=50, default='', editable=True)
+    email = models.EmailField(max_length=100, default='', editable=True)
+    unclas_email = models.EmailField(max_length=100, default='', editable=True)
     class_phone = models.CharField(max_length=30, default='', editable=True)
     unclas_phone = models.CharField(max_length=30, default='', editable=True)
-    company = models.CharField(max_length=30, default='', editable=True)
+    company = models.CharField(max_length=50, default='', editable=True)
     subscribe_to_emails = models.BooleanField(default=True, editable=True)
 
 
     def __str__(self):
         return self.username
+
+    def profile_complete(self):
+        return bool(self.first_name and self.last_name and self.email and self.unclas_email
+                    and self.class_phone and self.unclas_phone and self.company)
 
 
 class Contract(models.Model):
