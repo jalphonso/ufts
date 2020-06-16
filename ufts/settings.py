5c0fe52138116d0f0788d0a21f5e18da81840838
Joe Alphonso <jalphonso@juniper.net>
2020-06-16 17:34:07 -0400

    removed unneeded config line

--- ufts/settings.py
+++ ufts/settings.py
@@ -216,8 +216,6 @@ MEDIA_URL = '/media/'
 SOFTWARE_ROOT = '/opt/services/ufts/software/'
 SOFTWARE_URL = '/software/'
 
-FILE_UPLOAD_TEMP_DIR = SOFTWARE_ROOT
-
 ## 16GB File will be loaded in memory with this setting
 ## Only use if experiencing poor disk performance
 # FILE_UPLOAD_MAX_MEMORY_SIZE = 1024*1024*1024*16
